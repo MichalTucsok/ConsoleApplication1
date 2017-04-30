bbb68cdb79bb8df4105bd5f63fc45684d4fe16c2
MichalTucsok <tucsokm@gmail.com>
2017-04-30 12:04:52 +0200

    Final

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -1,339 +1,304 @@
-﻿/*
+﻿
+
+/////////////////////////////////////////////////////////////////////////
+
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/videoio.hpp"
+#include <ctype.h>
 #include "opencv2/opencv.hpp"
-#include<sstream>
-using namespace cv;
-using namespace std;
-int main(int, char**)
-{
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
-	cvNamedWindow("mywindow", CV_WINDOW_AUTOSIZE);
-	int ct=0;
-	for (;;)
-	{
-		Mat frame;
-		cap >> frame; // get a new frame from camera
-					  // do any processing
-		imshow("mywindow", frame);
-		
-
-//		String filename = "cropped_" + (ct + 1);
-//		imwrite(filename + ".jpg", frame);
-
-		
-		
-			// ...
-
-			// saving part:
-			//std::string savingName = "C:\\Users\\Michal\\Documents\\Visual Studio 2015/" + std::to_string(cntr) + ".jpg";
-			//cv::imwrite(savingName, frame);
-			//cntr++;
-		//String filename = "cropped_" + cntr;
-		//imwrite("C:\\Users\\Michal\\Documents\\Visual Studio 2015/" +filename + ".jpg", frame);
-	    //cntr++;
-			stringstream ss;
-
-string name = "image_";
-string type = ".jpg";
-
-ss<<name<<(ct + 1)<<type;
-
-string filename = ss.str();
-ss.str("");
-
-//imwrite(filename, frame);
-			//imwrite("C:\\Users\\Michal\\Documents\\Visual Studio 2015/"+cntr + ".bmp", frame);
-		
-		imwrite("C:\\Users\\Michal\\Documents\\Visual Studio 2015/"+ filename + ".bmp", frame);
-		ct++;
-		if (ct == 10) break;
-		if (waitKey(30) >= 0) break;   // you can increase delay to 2 seconds here
-	}
-
-	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
-}
-*/
-// 888888888888888888888888888888888888888888888888888888888888888888
-/*
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
+#include <sstream>
+#include <string>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv\cv.h>
+#include <windows.h>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
 
-void help(char** argv)
+const static int SENSITIVITY_VALUE = 20;
+
+static void help()
 {
-	cout << "\nThis program gets you started reading a sequence of images using cv::VideoCapture.\n"
-		<< "Image sequences are a common way to distribute video data sets for computer vision.\n"
-		<< "Usage: " << argv[0] << " <path to the first image in the sequence>\n"
-		<< "example: " << argv[0] << " right%%02d.jpg\n"
-		<< "q,Q,esc -- quit\n"
-		<< "\tThis is a starter sample, to get you up and going in a copy paste fashion\n"
-		<< endl;
+	// print a welcome message, and the OpenCV version
+	cout << "\nThis is a demo of Lukas-Kanade optical flow lkdemo(),\n"
+		"Using OpenCV version " << CV_VERSION << endl;
+	cout << "\nIt uses camera by default, but you can provide a path to video as an argument.\n";
+	cout << "\nHot keys: \n"
+		"\tESC - quit the program\n"
+		"\tr - auto-initialize tracking\n"
+		"\tc - delete all the points\n"
+		"\tn - switch the \"night\" mode on/off\n"
+		"To add/remove a feature point click it\n" << endl;
+}
+
+Point2f point;
+bool addRemovePt = false;
+
+static void onMouse(int event, int x, int y, int /*flags*/, void* /*param*/)
+{
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		point = Point2f((float)x, (float)y);
+		addRemovePt = true;
+	}
 }
 
 int main(int argc, char** argv)
 {
-	if (argc !=2)
+	//Create a black image with the size as the camera output
+	Mat imgLines;
+	Mat grayImage1, grayImage2;
+	Mat differenceImage;
+	Mat imgOriginal;
+	Mat thresholdImage;
+	Mat imgHSV;
+	Mat imgThresholded;
+	Mat imgTmp;
+	Mat imgTmp2;
+
+	Moments oMoments;
+
+	bool bSuccess;
+
+	double dM01;
+	double dM10;
+	double dArea;
+	double vzdialenost;
+
+	int posX;
+	int posY;
+	int iLastX = -1;
+	int iLastY = -1;
+
+
+	VideoCapture cap(0); //capture the video from webcam
+	TermCriteria termcrit(TermCriteria::COUNT | TermCriteria::EPS, 20, 0.03);
+	Size subPixWinSize(10, 10), winSize(31, 31);
+
+	const int MAX_COUNT = 500;
+	bool needToInit = false;
+	bool nightMode = false;
+
+	cv::CommandLineParser parser(argc, argv, "{@input||}{help h||}");
+	string input = parser.get<string>("@input");
+	/*
+	if (parser.has("help"))
 	{
-		help(argv);
-		return 1;
+		help();
+		return 0;
 	}
+	if (input.empty())
+		cap.open(0);
+	else if (input.size() == 1 && isdigit(input[0]))
+		cap.open(input[0] - '0');
+	else
+		cap.open(input);
 
-	string arg = argv[1];
-	VideoCapture sequence(arg);
-	if (!sequence.isOpened())
+	if (!cap.isOpened())
 	{
-		cerr << "Failed to open Image Sequence!\n" << endl;
-		return 1;
+		cout << "Could not initialize capturing...\n";
+		return 0;
 	}
+	*/
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+												//blue color detection settings
+	int iLowH = 80;
+	int iHighH = 130;
 
-	Mat image;
-	namedWindow("Image | q or esc to quit", CV_WINDOW_NORMAL);
+	int iLowS = 150;
+	int iHighS = 255;
 
-	for (;;)
+	int iLowV = 60;
+	int iHighV = 255;
+
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+	cap.read(imgTmp);	//temporary image from camera
+	imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);
+
+	vector<Vec3f> circles;
+	int radius;
+	Point center;
+
+
+	namedWindow("LK Demo", 1);
+	setMouseCallback("LK Demo", onMouse, 0);
+
+	Mat gray, prevGray, image, frame;
+	vector<Point2f> points[2];
+
+	//for (;;)
+	while(true)
 	{
-		sequence >> image;
-		if (image.empty())
+		// imgOriginal premenovane na frame //
+		bSuccess = cap.read(frame); // read a new frame from video
+
+		if (!bSuccess) { //if not success, break loop      
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+		frame.copyTo(image);
+		// grayImage1 premenovane na gray
+		cvtColor(image, gray, COLOR_BGR2GRAY);
+		
+
+		GaussianBlur(gray, gray, Size(9, 9), 2, 2);
+
+		/// Apply the Hough Transform to find the circles
+		HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 2, 20, 100, 155, 20, 300);
+		waitKey(100);
+		/// Draw the circles detected
+		for (size_t i = 0; i < circles.size(); i++) {
+			//center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			center.x = cvRound(circles[i][0]);
+			center.y = cvRound(circles[i][1]);
+
+			radius = cvRound(circles[i][2]);
+			// circle center
+			circle(image, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+			// circle outline
+			circle(image, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		}
+
+		imshow("Hough circles", image);
+
+		cvtColor(frame, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+																									  //morphological opening (removes small objects from the foreground)
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//morphological closing (removes small holes from the foreground)
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//Calculate the moments of the thresholded image
+		oMoments = moments(imgThresholded);
+
+		dM01 = oMoments.m01;
+		dM10 = oMoments.m10;
+		dArea = oMoments.m00;
+
+		// if the area <= 10000, I consider that there are no object in the image and it's because of the noise, the area is not zero 
+		if (dArea > 10000) {
+			//calculate the position of the ball
+			posX = dM10 / dArea;
+			posY = dM01 / dArea;
+
+			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
+				//Draw a red line from the previous point to the current point
+				line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
+				//std::cout << "X: " << (int)(iLastX-posX) << "     Y: " << (int)(iLastY-posY) << endl;   //pocitam si vzdialenost
+				//vzdialenost = sqrt((double)((iLastX - posX)*(iLastX - posX)) + (double)((iLastY - posY)*(iLastY - posY)));
+				//cout << vzdialenost  << endl;
+				//cout << "\n cm\n" << endl;
+				vzdialenost = 2.2 / radius * 1000;
+				cout << vzdialenost << endl;
+				cout << "\n cm\n" << endl;
+				
+					
+					//printf("Z os %f", 2.2 / radius * 1000);
+				//Sleep(2000);
+			}
+
+			iLastX = posX;
+			iLastY = posY;
+		}
+
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+		frame = frame + imgLines;
+		imshow("Original", frame); //show the original image
+
+		
+
+
+		if (nightMode)
+			image = Scalar::all(0);
+
+		if (needToInit)
 		{
-			cout << "End of Sequence" << endl;
+			// automatic initialization
+			goodFeaturesToTrack(gray, points[1], MAX_COUNT, 0.01, 10, Mat(), 3, 0, 0.04);
+			cornerSubPix(gray, points[1], subPixWinSize, Size(-1, -1), termcrit);
+			addRemovePt = false;
+		}
+		else if (!points[0].empty())
+		{
+			vector<uchar> status;
+			vector<float> err;
+			if (prevGray.empty())
+				gray.copyTo(prevGray);
+			calcOpticalFlowPyrLK(prevGray, gray, points[0], points[1], status, err, winSize,
+				3, termcrit, 0, 0.001);
+			size_t i, k;
+			for (i = k = 0; i < points[1].size(); i++)
+			{
+				if (addRemovePt)
+				{
+					if (norm(point - points[1][i]) <= 5)
+					{
+						addRemovePt = false;
+						continue;
+					}
+				}
+
+				if (!status[i])
+					continue;
+
+				points[1][k++] = points[1][i];
+				circle(image, points[1][i], 3, Scalar(0, 255, 0), -1, 8);
+			}
+			points[1].resize(k);
+		}
+
+		if (addRemovePt && points[1].size() < (size_t)MAX_COUNT)
+		{
+			vector<Point2f> tmp;
+			tmp.push_back(point);
+			cornerSubPix(gray, tmp, winSize, Size(-1, -1), termcrit);
+			points[1].push_back(tmp[0]);
+			addRemovePt = false;
+		}
+
+		needToInit = false;
+		////////////////
+		imshow("LK Demo", image);
+
+		char c = (char)waitKey(10);
+		if (c == 27)
+			break;
+		switch (c)
+		{
+		case 'r':
+			needToInit = true;
+			break;
+		case 'c':
+			points[0].clear();
+			points[1].clear();
+			break;
+		case 'n':
+			nightMode = !nightMode;
 			break;
 		}
 
-		imshow("image | q or esc to quit", image);
-
-		char key = (char)waitKey(500);
-		if (key == 'q' || key == 'Q' || key == 27)
-			break;
+		std::swap(points[1], points[0]);
+		cv::swap(prevGray, gray);
 	}
 
 	return 0;
-}*/
-//8888888888888888888888888888888888888888888888888888888888888888888888
-/*
-#include <iostream>
-#include <sstream>
-#include <string.h>
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/video/video.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/video/tracking.hpp>
-#include <opencv2/legacy/legacy.hpp>
-#include <cmath>
-#include <vector>
-
-using namespace std;
-using namespace cv;
-
-int main()
-{
-	int count;
-	double area, ar;
-	Mat frame, fore, img, prevImg, temp, gray, vehicle_ROI, img_temp;
-	VideoCapture cap("/Users/alien/Documents/Traffic/trafficdb/video/cctv052x2004080516x01638.avi");
-	BackgroundSubtractorMOG2 bg(500, 25, false);
-	vector<vector<Point> > contours;
-	vector<Rect> cars;
-	namedWindow("Frame");
-
-	cap >> img_temp;
-	cvtColor(img_temp, gray, CV_BGR2GRAY);
-	gray.convertTo(temp, CV_8U);
-	bilateralFilter(temp, prevImg, 5, 20, 20);
-
-	while (true)
-	{
-		count = 0;
-		cap >> frame;
-
-		cvtColor(frame, gray, CV_BGR2GRAY);
-		gray.convertTo(temp, CV_8U);
-		bilateralFilter(temp, img, 5, 20, 20);
-		bg.operator()(img, fore);
-		erode(fore, fore, Mat());
-		dilate(fore, fore, Mat());
-		findContours(fore, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-		vector<vector<Point> > contours_poly(contours.size());
-		vector<Rect> boundRect(contours.size());
-
-		for (size_t i = 0; i < contours.size(); i++)
-		{
-			approxPolyDP(Mat(contours[i]), contours_poly[i], 10, true);
-			boundRect[i] = boundingRect(Mat(contours_poly[i]));
-			rectangle(frame, boundRect[i].tl(), boundRect[i].br(), Scalar(255, 0, 0), 2, 8, 0);
-
-			vehicle_ROI = img(boundRect[i]);
-			area = contourArea(contours[i], false);
-			ar = vehicle_ROI.cols / vehicle_ROI.rows;
-			if (area > 450.0 && ar > 0.8)
-			{
-				count = count + 1;
-			}
-		}
-		stringstream ss;
-		ss << count;
-		string s = ss.str();
-		int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
-		double fontScale = 2;
-		int thickness = 3;
-		cv::Point textOrg(10, 130);
-		cv::putText(frame, s, textOrg, fontFace, fontScale, Scalar(0, 255, 0), thickness, 5);
-
-		int win_size = 10;
-		int maxCorners = 200;
-		double qualityLevel = 0.01;
-		double minDistance = 1;
-		int blockSize = 3;
-		double k = 0.04;
-		vector<Point2f> img_corners;
-		img_corners.reserve(maxCorners);
-		vector<Point2f> prevImg_corners;
-		prevImg_corners.reserve(maxCorners);
-
-		goodFeaturesToTrack(img, img_corners, maxCorners, qualityLevel, minDistance, Mat(), blockSize, true);
-
-		cornerSubPix(img, img_corners, Size(win_size, win_size), Size(-1, -1),
-			TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, 0.03));
-
-		vector<uchar> features_found;
-		features_found.reserve(maxCorners);
-		vector<float> feature_errors;
-		feature_errors.reserve(maxCorners);
-
-		calcOpticalFlowPyrLK(img, prevImg, img_corners, prevImg_corners, features_found, feature_errors,
-			Size(win_size, win_size), 3,
-			cvTermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, 0.3), 0, k);
-
-		for (int i = 0; i < features_found.size(); i++) {
-
-			Point2f p0(ceil(img_corners[i].x), ceil(img_corners[i].y));
-			Point2f p1(ceil(prevImg_corners[i].x), ceil(prevImg_corners[i].y));
-			line(frame, p0, p1, CV_RGB(255, 0, 0), 5);
-		}
-
-		prevImg = img;
-
-		imshow("Frame", frame);
-
-		if (waitKey(5) >= 0)
-			break;
-	}
-
-}
-*/
-
-#include "opencv2\highgui.hpp"
-#include "opencv2\imgproc.hpp"
-#include "opencv2\objdetect\objdetect.hpp"
-#include "opencv2/video/tracking.hpp"
-#include <vector>
-#include <stdio.h>
-#include <Windows.h>
-#include <iostream>
-
-
-using namespace cv;
-using namespace std;
-
-
-
-int main(int argc, const char** argv)
-{
-
-	// add your file name
-	VideoCapture cap(1);
-
-
-	Mat flow, frame;
-	// some faster than mat image container
-	UMat  flowUmat, prevgray;
-
-	for (;;)
-	{
-
-		bool Is = cap.grab();
-		if (Is == false) {
-			// if video capture failed
-			cout << "Video Capture Fail" << endl;
-			break;
-		}
-		else {
-
-
-
-			Mat img;
-			Mat original;
-
-			// capture frame from video file
-			cap.retrieve(img, CV_CAP_OPENNI_BGR_IMAGE);
-			resize(img, img, Size(640, 480));
-
-			// save original for later
-			img.copyTo(original);
-
-			// just make current frame gray
-			cvtColor(img, img, COLOR_BGR2GRAY);
-
-
-			// For all optical flow you need a sequence of images.. Or at least 2 of them. Previous                           //and current frame
-			//if there is no current frame
-			// go to this part and fill previous frame
-			//else {
-			// img.copyTo(prevgray);
-			//   }
-			// if previous frame is not empty.. There is a picture of previous frame. Do some                                  //optical flow alg. 
-
-			if (prevgray.empty() == false) {
-
-				// calculate optical flow 
-				calcOpticalFlowFarneback(prevgray, img, flowUmat, 0.4, 1, 12, 2, 8, 1.2, 0);
-				// copy Umat container to standard Mat
-				flowUmat.copyTo(flow);
-
-
-				// By y += 5, x += 5 you can specify the grid 
-				for (int y = 0; y < original.rows; y += 10) {
-					for (int x = 0; x < original.cols; x += 10)
-					{
-						// get the flow from y, x position * 10 for better visibility
-						const Point2f flowatxy = flow.at<Point2f>(y, x) * 10;
-						// draw line at flow direction
-						line(original, Point(x, y), Point(cvRound(x + flowatxy.x), cvRound(y + flowatxy.y)), Scalar(255, 0, 0));
-						// draw initial point
-						circle(original, Point(x, y), 1, Scalar(0, 0, 0), -1);
-
-
-					}
-
-				}
-
-				// draw the results
-				namedWindow("prew", WINDOW_AUTOSIZE);
-				imshow("prew", original);
-
-				// fill previous image again
-				img.copyTo(prevgray);
-
-			}
-			else {
-
-				// fill previous image in case prevgray.empty() == true
-				img.copyTo(prevgray);
-
-			}
-
-
-			int key1 = waitKey(20);
-
-		}
-	}
 }
